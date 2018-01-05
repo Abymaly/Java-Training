4ef72857703c7af1a460283edc47a99fcc9b7ac3
Abymaly <ada.banaszczyk@gmail.com>
2018-01-05 20:59:17 +0100

    New code!

--- src/homework/Main.java
+++ src/homework/Main.java
@@ -4,7 +4,8 @@ import homework.homework02.Person;
 import homework.homework02.Student;
 import homework.homework02.Instructor;
 import homework.homework02.Employee;
-
+import homework.homework02.Salary;
+import homework.homework02.Faculty ;
 
 public class Main {
 
