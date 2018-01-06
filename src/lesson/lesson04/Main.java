4e7758b7f3d191d456cd61534fbe3253f6c6cbae
Abymaly <ada.banaszczyk@gmail.com>
2018-01-06 14:17:59 +0100

    Notatki z markdown

---
+++ src/lesson/lesson04/Main.java
@@ -0,0 +1,12 @@
+package lesson.lesson04;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // jak sie napisze, to tu bedzie kod
+        int x = 5;
+        if (x != 8) {
+            System.out.println("OK");
+        }
+    }
+}
