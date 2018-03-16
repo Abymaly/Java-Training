8cef4b71c42d14fc29e201d682fe65f0774ce53c
Abymaly <ada.banaszczyk@gmail.com>
2018-03-16 23:55:56 +0100

    Nie rozwiązuje to jeszcze problemu słów które występują jednakowo często, ale działa ;) I wartość końcowa jest zapisywana do obiektu.

--- src/homework/readingFile/Main.java
+++ src/homework/readingFile/Main.java
@@ -10,54 +10,83 @@ public class Main {
     public static void main(String[] args) {
 
         File file = new File("C:\\Users\\Ada\\Desktop\\Java Training\\homework\\txt.txt");
+        String txtLine = null;
+        StringBuilder allLine = new StringBuilder();
 
         try (
                 FileReader fileReader = new FileReader(file);
                 BufferedReader bufferedFileReader = new BufferedReader(fileReader);
         ) {
-
-            String txtLine = null;
-            StringBuilder allLine = new StringBuilder();
-
             while ((txtLine = bufferedFileReader.readLine()) != null) {
                 allLine.append(txtLine)
-                .append(" ");
-                // TODO: usunąć . Ale przy użyciu metody remove() nie wczytuje wtedy w ogóle tekstu
+                        .append(" ");
             }
 
-            String allLineStr = allLine.toString();
-            String allLineStrTxt = allLineStr.replaceAll(",", " ");
-            String [] allLineWord = allLineStrTxt.split(" ");
-
-            Map<String, Integer> wordsCount = new HashMap<>();
-            int actCount = 0;
-
-            for (String oneWord : allLineWord) {
-                if (oneWord.length() > 3) {
-                    if (wordsCount.get(oneWord) == null) {
-                        actCount = 0;
-                    }
-                    wordsCount.put(oneWord, ++actCount);
-                }
-            }
-
-            Set<Map.Entry<String, Integer>> entrySet = wordsCount.entrySet();
-
-            String maxWord = "";
-            Integer maxCount = 0;
-
-            for (Map.Entry<String, Integer> maxOcc : entrySet) {
-                if (maxOcc.getValue() > maxCount) {
-                    maxCount = maxOcc.getValue();
-                    maxWord = maxOcc.getKey();
-                }
-            }
-            System.out.println(maxWord + ": " + maxCount);
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (FileNotFoundException e1) {
+            e1.printStackTrace();
+        } catch (IOException e1) {
+            e1.printStackTrace();
         }
+
+        String allLineStr = allLine.toString();
+        String allLineStrTxt = allLineStr.replaceAll(",", " ");
+        // TODO: usunąć . Ale przy użyciu metody remove() nie wczytuje wtedy w ogóle tekstu
+        String[] allLineWord = allLineStrTxt.split(" ");
+
+        Map<String, Integer> wordsCount = new HashMap<>();
+
+        for (String oneWord : allLineWord) {
+
+            int actCount = 0;
+            if (oneWord.length() > 3) {
+                if (wordsCount.containsKey(oneWord) == false){
+                    wordsCount.put(oneWord, 1);
+                } else {
+                    actCount = wordsCount.get(oneWord) + 1;
+                    wordsCount.put(oneWord, actCount);
+                }
+            }
+        }
+
+        class Occurency {
+            String name;
+            Integer occ;
+
+            public Occurency() {
+            }
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+
+            public Integer getOcc() {
+                return occ;
+            }
+
+            public void setOcc(Integer occ) {
+                this.occ = occ;
+            }
+        }
+
+        Occurency winner = new Occurency();
+
+        Set<Map.Entry<String, Integer>> entrySet = wordsCount.entrySet();
+
+        Integer maxCount = 0;
+
+        for (Map.Entry<String, Integer> maxOcc : entrySet) {
+            if (maxOcc.getValue() > maxCount) {
+                maxCount = maxOcc.getValue();
+                winner.setName(maxOcc.getKey());
+                winner.setOcc(maxOcc.getValue());
+            }
+        }
+
+        System.out.println(winner.getName() + " : " + winner.getOcc());
+
     }
 }
