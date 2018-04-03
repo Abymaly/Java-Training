0f9c2c604fab8c1f324c72f9dd9b722996e34ab2
Abymaly <ada.banaszczyk@gmail.com>
2018-04-04 00:05:53 +0200

    Kalendarz, podejście drugie

--- src/homework/calendar/CalendarNew.java
+++ src/homework/calendar/CalendarNew.java
@@ -9,14 +9,14 @@ public class CalendarNew {
     public static void main(String[] args) {
 
         LocalDate todayDate = LocalDate.now();
-
-        //int actYear = todayDate.getYear();
-        //int actMonth = todayDate.getMonthValue();
         int actDay = todayDate.getDayOfMonth();
 
-        int daysInMonth = todayDate.lengthOfMonth();
+        //int daysInMonth = todayDate.lengthOfMonth();
 
-        System.out.println ("\n" + Month.of(todayDate.getMonthValue()).toString() + " " + todayDate.getYear() + "\n");
+        System.out.println("\n" + Month.of(todayDate.getMonthValue()).toString() + " " + todayDate.getYear() + "\n");
+
+        int columnWidth = 11;
+        //TODO: zamienić 11 na wartość odczytaną z długości najdłuższej nazwy
 
         DayOfWeek[] dayOfWeek = DayOfWeek.values();
 
@@ -25,73 +25,34 @@ public class CalendarNew {
             System.out.format("%11s", days.name());
         }
 
-        int columnWidth = 11;
-
         String newLine = "\n";
         StringBuffer sbLine = new StringBuffer(newLine);
-        for(int i = 0; i != dayOfWeek.length*columnWidth; ++i) {
+        for (int i = 0; i != dayOfWeek.length * columnWidth; ++i) {
             sbLine.append('-');
         }
         sbLine.append("\n");
 
-        System.out.println(sbLine.toString());
+        System.out.print(sbLine.toString());
 
+        LocalDate daysOfMonth = todayDate.minusDays(actDay - 1);
 
+        //TODO zmienić "%11s" na przekazanie wartości ze zmiennej columnWidth
 
+        for (int i = 1; i != daysOfMonth.getDayOfWeek().getValue(); ++i) {
+            System.out.format("%11s", " ");
+        }
 
-
-
-        // LocalDate monthBegin = todayDate.minusDays(actDay - 1);
-        // LocalDate monthEnd = (monthBegin.plusMonths(1)).minusDays(1);
-
-        // int firstDay = monthBegin.getDayOfMonth();
-        // int lastDay = monthEnd.getDayOfMonth();
-
-
-        // DayOfWeek beginWeekday = monthBegin.getDayOfWeek();
-        // int valueBeginWeekday = beginWeekday.getValue();
-
-
-        // int[][] calendar;
-        // calendar = new int[weekdaysValue][7];
-
-        // for (int i = 0; i != calendar[0].length; ++i) {
-        //     calendar[0][i] = i + 1;
-        // }
-
-        // for (int i = valueBeginWeekday - 1; i != calendar[0].length; ++i) {
-        //     calendar[1][i] = firstDay;
-        //     firstDay++;
-        // }
-
-        // for (int i = 2; i != calendar.length; ++i) {
-        //     for (int j = 0; j != calendar[0].length; ++j) {
-        //         calendar[i][j] = firstDay;
-        //         firstDay++;
-        //     }
-        // }
-
-        // System.out.println("\n" + Month.of(actMonth).toString() + "   " + actYear + "\n");
-
-        // for (int i = 0; i != calendar[0].length; ++i) {
-        //     System.out.format("%11s", DayOfWeek.of((calendar[0][i])).toString());
-        // }
-        // System.out.println("\n-------------------------------------------------------------------------------");
-
-        // for (int i = 1; i != calendar.length; ++i) {
-        //     for (int j = 0; j != calendar[0].length; ++j) {
-        //         if ((calendar[i][j] == 0) || (calendar[i][j] > lastDay)) {
-        //             System.out.format("%11s", "");
-        //         } else if (calendar[i][j] == actDay) {
-        //             System.out.format("%8s", "[" + calendar[i][j] + "]");
-        //             System.out.print("   ");
-        //         } else {
-        //             System.out.format("%8s", calendar[i][j]);
-        //             System.out.print("   ");
-        //         }
-        //     }
-        //     System.out.println("\n-------------------------------------------------------------------------------");
-        // }
+        while (daysOfMonth.getMonthValue() == todayDate.getMonthValue()) {
+            if (daysOfMonth.equals(todayDate)) {
+                System.out.format("%11s", "[" + daysOfMonth.getDayOfMonth() + "]");
+            } else {
+                System.out.format("%11s", daysOfMonth.getDayOfMonth());
+            }
+            if (daysOfMonth.getDayOfWeek().getValue() % dayOfWeek.length == 0) {
+                System.out.print(sbLine.toString());
+            }
+            daysOfMonth = daysOfMonth.plusDays(1);
+        }
+        System.out.print(sbLine.toString());
     }
 }
-
